6c95095df0742b38c72bf9ad842818320e6398c9
19julio94 <juliopatriciodasilva@gmail.com>
2018-05-14 20:32:28 +0200

    Anhadido contructor XML y toDOM,sin acabar

--- PROGRAMACION/BLOQUE III/GestClinica3/src/es/uvigo/esei/pro2/core/Hora.java
+++ PROGRAMACION/BLOQUE III/GestClinica3/src/es/uvigo/esei/pro2/core/Hora.java
@@ -4,16 +4,23 @@
 
 package es.uvigo.esei.pro2.core;
 
+import nu.xom.Element;
+import nu.xom.ParsingException;
+
 /**
- *
  * @author Nani
  */
 public class Hora {
+    public static final String tagHora = "hora";
+    public static final String tagMinutos = "minutos";
+
     private int hora;
     private int minutos;
 
-    /** Crea una nueva hora, con sus datos: hora y minutos
-     * @param hora hora
+    /**
+     * Crea una nueva hora, con sus datos: hora y minutos
+     *
+     * @param hora    hora
      * @param minutos minutos
      */
     public Hora(int hora, int minutos) {
@@ -21,44 +28,106 @@ public class Hora {
         this.minutos = minutos;
     }
 
-    /** Devuelve la hora 
+    public Hora(Element e) throws ParsingException {
+
+        Element eltoHora = e.getFirstChildElement(tagHora);
+        Element eltoMinutos = e.getFirstChildElement(tagMinutos);
+
+        if (eltoHora == null) {
+
+            throw new ParsingException("Falta la hora");
+
+        }
+
+        if (eltoMinutos == null) {
+
+            throw new ParsingException("Faltan los minutos");
+        }
+
+
+        try {
+
+            this.hora = Integer.parseInt(eltoHora.getValue().trim());
+        } catch (NumberFormatException exc) {
+
+            throw new ParsingException("Valor para la hora incorrecto");
+
+        }
+
+        try {
+            this.minutos = Integer.parseInt(eltoMinutos.getValue().trim());
+        } catch (NumberFormatException exc) {
+
+            throw new ParsingException("Valor para los minutos incorrecto");
+
+        }
+
+
+    }
+
+    /**
+     * Devuelve la hora
+     *
      * @return la hora, como int.
-     */ 
+     */
     public int getHora() {
         return hora;
     }
-    
-    /** Devuelve los minutosss
+
+    /**
+     * Devuelve los minutosss
+     *
      * @return los minutos, como int.
-     */ 
+     */
     public int getMinutos() {
         return minutos;
     }
 
-   /** Cambia la hora
+    /**
+     * Cambia la hora
+     *
      * @param hora la hora
-     */  
+     */
     public void setHora(int hora) {
         this.hora = hora;
     }
-    
-   /** Cambia los minutos
+
+    /**
+     * Cambia los minutos
+     *
      * @param minutos los minutos
-     */  
+     */
     public void setMinutos(int minutos) {
         this.minutos = minutos;
     }
-    
+
+     public Element toDOM(){
+
+        Element root = new Element(tagHora);
+        Element eltoHora = new Element(tagHora);
+        Element eltoMinutos = new Element(tagMinutos);
+
+        eltoHora.appendChild(Integer.toString(getHora()));
+        eltoMinutos.appendChild(Integer.toString(getMinutos()));
+
+        root.appendChild(eltoHora);
+        root.appendChild(eltoMinutos);
+
+        return root;
+
+
+
+        }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         StringBuilder toret = new StringBuilder();
-        
+
         toret.append("(").append(getHora())
-             .append(":").append(getMinutos())
-             .append(")");
-             
+                .append(":").append(getMinutos())
+                .append(")");
+
         return toret.toString();
-    } 
-    
+    }
+
 }
--- PROGRAMACION/BLOQUE III/GestClinica3/src/es/uvigo/esei/pro2/core/Privado.java
+++ PROGRAMACION/BLOQUE III/GestClinica3/src/es/uvigo/esei/pro2/core/Privado.java
@@ -4,11 +4,18 @@
 
 package es.uvigo.esei.pro2.core;
 
+import nu.xom.Element;
+import nu.xom.ParsingException;
+
 /**
  * Paciente que asiste a la clínica de forma privada
  * Created by nanny on 8/03/2018.
  */
 public class Privado extends Paciente {
+
+    public static final String tagPrivadp = "privado";
+    public static final String tagDni = "dni";
+
     private String dni;
    
      /** Crea un nuevo paciente que va a la clínica por privado, 
@@ -26,6 +33,22 @@ public class Privado extends Paciente {
         this.setDni(dni);
     }
 
+    public Privado(Element e) throws ParsingException {
+
+        super(e);
+        Element eltoDni = e.getFirstChildElement(tagDni);
+
+        if(eltoDni == null){
+
+            throw new ParsingException("falta el DNI");
+
+        }
+
+        this.dni = eltoDni.getValue().trim();
+
+
+    }
+
     /** Devuelve el D.N.I. del paciente
      * @return el D.N.I. del paciente, como String.
      */ 
@@ -38,6 +61,23 @@ public class Privado extends Paciente {
      */     
     public void setDni(String dni) {
         this.dni = dni;
+    }
+
+
+    public Element toDOM(){
+
+        Element root = super.toDOM();
+        root.setLocalName(tagPrivadp);
+        Element eltoDni = new Element(tagDni);
+
+        eltoDni.appendChild(tagDni);
+
+        root.appendChild(eltoDni);
+
+        return root;
+
+
+
     }
     
     @Override
